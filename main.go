e4ee86dfcd478cdf71e2312f5a7c873a8cf0cc61
Gerald Philip MacKenzie <gm@flowfaction.com>
2016-01-03 14:00:03 -0400

    refactoring. not sure i like the OO approach, but at least it makes it easy to develop for a while. adding number assertions, but not working yet.

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	log "github.com/Sirupsen/logrus"
 	"encoding/json"
+	log "github.com/Sirupsen/logrus"
 	"strings"
+	"reflect"
 )
 
 func init() {
@@ -12,8 +13,38 @@ func init() {
 
 
 
-func SubMapNodeExists(path []string, receptacle map[string]interface{}) (found bool) {
+type JsonAssertion struct {
+	//	Path string
+	Ok         bool
+	json       string
+	receptacle map[string]interface{}
+	decoder    *json.Decoder
+}
 
+func MakeJsonAssertion(data string) (jas *JsonAssertion, err error) {
+	jas = &JsonAssertion{json: data,receptacle:make(map[string]interface{})}
+	jas.decoder = json.NewDecoder(strings.NewReader(jas.json))
+	if err = jas.decoder.Decode(&jas.receptacle); err != nil {
+		log.Errorf("decoding error %v", err.Error())
+	}
+	return
+}
+
+func (jas *JsonAssertion) AssertNumberAtPath(number int, path string) bool {
+	nodes := strings.Split(path, "/")[1:] // discard the first empty slot due to leading '/'
+	jas.Ok = jas.jsonNumberExists(nodes,jas.receptacle)
+	return jas.Ok
+}
+
+func (jas *JsonAssertion) AssertObjectAtPath(path string) bool {
+	nodes := strings.Split(path, "/")[1:] // discard the first empty slot due to leading '/'
+	jas.Ok = jas.jsonObjectExists(nodes,jas.receptacle)
+	return jas.Ok
+}
+
+func (jas *JsonAssertion) jsonObjectExists(path []string, receptacle map[string]interface{}) (found bool) {
+
+	// local func to clean up the recursion conditional
 	// returns true if the key exists and its value is a submap[string]interface{}
 	key_and_map := func(key string, m map[string]interface{}) (submap map[string]interface{}, foundkm bool) {
 		if sub, ok := m[key]; ok {
@@ -24,10 +55,10 @@ func SubMapNodeExists(path []string, receptacle map[string]interface{}) (found b
 		return
 	}
 
-	var sub_map map[string]interface{}
+	var sub_map map[string]interface{} // declare this var outside of condition, so we can populate the retvar
 	if sub_map, found = key_and_map(path[0], receptacle); found {
 		if len(path) > 1 {
-			return SubMapNodeExists(path[1:], sub_map)
+			return jas.jsonObjectExists(path[1:], sub_map)
 		} // otherwise just return the value of 'found'
 	} else {
 		log.Debugf("key (%v) not found", path[0])
@@ -35,6 +66,35 @@ func SubMapNodeExists(path []string, receptacle map[string]interface{}) (found b
 
 	return
 }
+
+func (jas *JsonAssertion) jsonNumberExists(path []string, receptacle map[string]interface{}) (found bool) {
+
+	// local func to clean up the recursion conditional
+	// returns true if the key exists and its value is a submap[string]interface{}
+	key_and_number := func(key string, m map[string]interface{}) (submap map[string]interface{}, foundkm bool) {
+		if sub, ok := m[key]; ok {
+			log.Debugf("type at node [%v]",reflect.TypeOf(sub))
+			submap, foundkm = sub.(map[string]interface{})
+		} else {
+			log.Debugf("key not found in map (%v)", key)
+		}
+		return
+	}
+
+	var sub_map map[string]interface{} // declare this var outside of condition, so we can populate the retvar
+	if sub_map, found = key_and_number(path[0], receptacle); found {
+		if len(path) > 1 {
+			return jas.jsonNumberExists(path[1:], sub_map)
+		} // otherwise just return the value of 'found'
+	} else {
+		log.Debugf("key (%v) not found", path[0])
+	}
+
+	return
+}
+
+
+
 
 func main() {
 
@@ -75,21 +135,19 @@ func main() {
 			}
 	}`
 
-	dec := json.NewDecoder(strings.NewReader(json_data))
+	jas, _ := MakeJsonAssertion(json_data)
 
-	var receptacle map[string]interface{}
-	if err := dec.Decode(&receptacle); err != nil {
-		log.Fatalf("decoding error %v", err.Error())
-		//todo: if this error happens, assertion fails, obviously. but don't kill the program
-	}
-
+	var ok bool
 	path := "/user/properties/object/value"
+	ok = jas.AssertObjectAtPath(path)
+	log.Debugf("object found at path [%v], ok=%v",path,ok)
 
-	nodes := strings.Split(path, "/")[1:] // discard the first empty slot due to leading '/'
+	jas, _ = MakeJsonAssertion(json_data)
+	path = "/user/properties/object/value/baz"
+	ok = jas.AssertNumberAtPath(11235,"/user/properties/object/value/baz")
+	log.Debugf("number found at path [%v], ok=%v",path,ok)
 
-	// the slice of strings is depth-first navigation of the series of map[string]interface{} parsed by the decoder
-	// at each node, represented by a key in the map, the next nested map is checked
-	log.Debugf("node found at path[%s]=%v", path, SubMapNodeExists(nodes, receptacle))
+
+
 
 }
-
