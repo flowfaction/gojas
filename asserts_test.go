6aea394da6019da27a6e73cf62ddf16f49759adf
Gerald Philip MacKenzie <gm@flowfaction.com>
2016-02-03 22:39:00 -0500

    all tests of tests are passing, with verification of Failed() for testing.T

--- asserts_test.go
+++ asserts_test.go
@@ -120,121 +120,105 @@ func TestAssertNumber(t *testing.T) {
 
 }
 
-//func TestAssertFloatArray(t *testing.T) {
-//	original_val := []interface{}{1.0, 1.0, 2.0, 3.0, 5.0, 8.0}
-//
-//	path := "/user/properties/numberArray/value"
-//	val := original_val
-//
-//	if !AssertFloatArrayAtPath(t,asserted_json_data,path,val) {
-//		t.Fatal("Failed to pass asserted float array at known path")
-//	}
-//
-//	val = append(val,9.0)
-//	if AssertFloatArrayAtPath(t,asserted_json_data,path,val){
-//		t.Fatal("Failed to fail assert of modified float array at known path")
-//	}
-//
-//	sim := clone(t)
-//	_ = AssertFloatArrayAtPath(sim,bad_json_data,path,val)
-//	if !sim.Failed() { // should have marked test as failed
-//		t.Logf("test assertion failed to detect parsing error on intentionally bad data. fail!")
-//		t.Fatal("Failed to fail test when detecting parse error or failing to make a JsonAssertion")
-//	} else {
-////		// it did fail, but we want to reset the status for the purposes of this outer self-test
-//		t.Logf("test assertion had error parsing intentionally bad json data, as expected. win!")
-//	}
-//
-//}
-//
-//func TestAssertStringArray(t *testing.T) {
-//
-//	original_val := []interface{}{"1","1","2","3","5","8"}
-//	val := original_val
-//
-//	path := "/user/properties/stringArray/value"
-//	if !AssertStringArrayAtPath(t,asserted_json_data,path,val) {
-//		t.Fatal("Failed to pass asserted string array at known path")
-//	}
-//
-//
-//	val = append(val,"9")
-//	if AssertStringArrayAtPath(t,asserted_json_data,path,val) {
-//		t.Fatal("Failed to fail assert of modified string array at known path")
-//	}
-//
-//	sim := clone(t)
-//	_ = AssertStringArrayAtPath(sim,bad_json_data,path,val)
-//	if !sim.Failed() { // should have marked test as failed
-//		t.Logf("test assertion failed to detect parsing error on intentionally bad data. fail!")
-//		t.Fatal("Failed to fail test when detecting parse error or failing to make a JsonAssertion")
-//	} else {
-////		// it did fail, but we want to reset the status for the purposes of this outer self-test
-//		t.Logf("test assertion had error parsing intentionally bad json data, as	 expected. win!")
-//	}
-//
-//
-//
-//}
-//
-//func TestAssertString(t *testing.T) {
-//	path := "/user/properties/string/value"
-//
-//	val := "foobar"
-//	if !AssertStringAtPath(t,asserted_json_data,path,val) {
-//		t.Fatal("Failed to pass assert string('foobar') at known path")
-//	}
-//
-//	if AssertStringAtPath(clone(t),asserted_json_data,path,"shouldnotbethere") {
-//		t.Fatal("AssertStringAtPath Failed to mark testing.T as failed when not finding 'shouldnotbethere' at known path")
-//	}
-//
-//	sim := clone(t)
-//	if AssertStringAtPath(sim,bad_json_data,path,val) {
-//		// bad. it should have returned false
-//		t.Logf("AssertStringAtPath failed to detect parsing error on intentionally bad data. fail!")
-//		t.Fatal("AssertStringAtPath failed to detect parse error or failed to make a JsonAssertion")
-//	} else {
-//		// but did it fail the T?
-//		if sim.Failed() {
-//			t.Logf("PASS: AssertStringAtPath correctly marked the testing.T as failed.")
-//		} else {
-//			t.Fatal("FAIL: AssertStringAtPath failed to mark testing.T as failed")
-//		}
-//	}
-//
-//}
-//
-//func TestAssertBool(t *testing.T) {
-//	path := "/user/properties/boolean/value"
-//
-//	if !AssertBoolAtPath(t,asserted_json_data,path,true) {
-//		t.Fatal("Failed to pass assert boolean(true) at known path")
-//	}
-//
-//	if AssertBoolAtPath(t,asserted_json_data,path,false) {
-//		t.Fatal("Failed to fail assert of not boolean(false) at known path")
-//	}
-//
-//	sim := clone(t)
-//	if AssertBoolAtPath(sim,bad_json_data,path,true) {
-//		// the assert should have caught the bad data
-//		t.Fatal("Failed to fail an assertion about a boolean[%v] at a non-existent path, in unparseable JSON.",true)
-//	} else {
-//		// the assert caught it - good - and returned false, but did it fail the 'T' ?
-//
-//	}
-//
-//
-//
-//	if !sim.Failed() { // should have marked test as failed
-//		t.Logf("test assertion failed to detect parsing error on intentionally bad data. fail!")
-//		t.Fatal("Failed to fail test when detecting parse error or failing to make a JsonAssertion")
-//	} else {
-////		// it did fail, but we want to reset the status for the purposes of this outer self-test
-//		t.Logf("test assertion had error parsing intentionally bad json data, as expected. win!")
-//	}
-//
-//
-//}
-//
+func TestAssertFloatArray(t *testing.T) {
+	original_val := []interface{}{1.0, 1.0, 2.0, 3.0, 5.0, 8.0}
+
+	path := "/user/properties/numberArray/value"
+	val := original_val
+
+	if !AssertFloatArrayAtPath(t,asserted_json_data,path,val) {
+		t.Fatal("AssertFloatArrayAtPath-Failed to pass asserted float array at known path")
+	}
+
+	val = append(val,9.0)
+	sim := *t
+	if AssertFloatArrayAtPath(&sim,asserted_json_data,path,val){
+		t.Fatal("AssertFloatArrayAtPath-Failed to fail assert of modified float array at known path")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+	sim = *t
+	if AssertFloatArrayAtPath(&sim,bad_json_data,path,val) {
+		t.Fatal("AssertFloatArrayAtPath-Failed to fail assertion against bad json data")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+}
+
+func TestAssertStringArray(t *testing.T) {
+
+	original_val := []interface{}{"1","1","2","3","5","8"}
+	val := original_val
+
+	path := "/user/properties/stringArray/value"
+	if !AssertStringArrayAtPath(t,asserted_json_data,path,val) {
+		t.Fatal("AssertStringArrayAtPath-Failed to pass asserted string array at known path")
+	}
+
+	val = append(val,"9")
+	sim := *t
+	if AssertStringArrayAtPath(&sim,asserted_json_data,path,val) {
+		t.Fatal("AssertStringArrayAtPath-Failed to fail assert of modified string array at known path")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+	sim = *t
+	if AssertStringArrayAtPath(&sim,bad_json_data,path,val) {
+		t.Fatal("AssertStringArrayAtPath-Failed to fail assertion against bad json data")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+
+}
+
+func TestAssertString(t *testing.T) {
+	path := "/user/properties/string/value"
+
+	val := "foobar"
+	if !AssertStringAtPath(t,asserted_json_data,path,val) {
+		t.Fatal("AssertStringAtPath-Failed to pass assert string('foobar') at known path")
+	}
+
+	sim := *t
+	if AssertStringAtPath(&sim,asserted_json_data,path,"shouldnotbethere") {
+		t.Fatal("AssertStringAtPath-Failed to mark testing.T as failed when not finding 'shouldnotbethere' at known path")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+	sim = *t
+	if AssertStringAtPath(&sim,bad_json_data,path,val) {
+		t.Fatal("AssertStringAtPath-Failed to fail assertion against bad json data")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+}
+
+func TestAssertBool(t *testing.T) {
+	path := "/user/properties/boolean/value"
+
+	if !AssertBoolAtPath(t,asserted_json_data,path,true) {
+		t.Fatal("AssertBoolAtPath-Failed to pass assert boolean(true) at known path")
+	}
+
+	sim := *t
+	if AssertBoolAtPath(&sim,asserted_json_data,path,false) {
+		t.Fatal("AssertBoolAtPath-Failed to fail assert of not boolean(false) at known path")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+	sim = *t
+	if AssertBoolAtPath(&sim,bad_json_data,path,true) {
+		t.Fatal("AssertBoolAtPath-Failed to fail assertion against bad json data")
+	} else {
+		assertSimFailed(sim,t)
+	}
+
+}
+
